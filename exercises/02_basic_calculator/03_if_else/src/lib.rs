da72b1c95a242da1c6a105f34b48434b4907c084
Dr. Nobel Khandaker <nobel@outlook.com>
2026-05-23 09:05:20 -0500

    update

--- exercises/02_basic_calculator/03_if_else/src/lib.rs
+++ exercises/02_basic_calculator/03_if_else/src/lib.rs
@@ -1,8 +1,15 @@
 /// Return `12` if `n` is even,
 /// `13` if `n` is divisible by `3`,
 /// `17` otherwise.
-fn magic_number(n: u32) -> u32 {
-    todo!()
+fn magic_number(n: u32) -> u32
+{
+     return if n%2==0 {
+         12
+     }else if n%3== 0 {
+         13
+     }else {
+         17
+     }
 }
 
 #[cfg(test)]
